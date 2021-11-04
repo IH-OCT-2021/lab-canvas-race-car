c7c974e5dc049a9e87383df5158d688d59eb625c
Connor McNeil <cnnrmcnl@gmail.com>
2021-11-04 17:01:43 -0400

    connor

--- js/index.js
+++ js/index.js
@@ -8,3 +8,22 @@ blueCar.src = 'images/car.png'
 blueCar.onload = () => {
     ctx.drawImage(blueCar, 150, canvas.height - 260, 158, 250)
 }
+
+let car = {
+    x: 150,
+    y: canvas.height-260,
+    w: 158,
+    h: 250,
+} 
+
+window.onkeydown = function (e) {
+    switch (e.key) {
+        case 'ArrowLeft':
+        car.x -= 50;
+            break;
+
+        case 'ArrowRight':
+        car.y += 50;
+        break
+    } 
+}
